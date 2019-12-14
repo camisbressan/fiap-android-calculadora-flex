fb7bfb6d64b95fd3258434fca6cff50213c4125a
Logon Aluno <logonaluno@local.com>
2019-12-14 11:14:32 -0300

    Projeto V1

---
+++ app/src/main/java/br/com/camila/calculadoraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.camila.calculadoraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.camila.calculadoraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
